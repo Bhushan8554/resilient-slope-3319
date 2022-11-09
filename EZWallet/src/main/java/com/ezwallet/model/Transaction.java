7ff867763c065d29b0bcde345ed81900c34d3d90
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2022-11-09 16:23:38 +0530

    some changes

--- EZWallet/src/main/java/com/ezwallet/model/Transaction.java
+++ EZWallet/src/main/java/com/ezwallet/model/Transaction.java
@@ -2,11 +2,20 @@ package com.ezwallet.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 import lombok.ToString;
 
 
+@Entity
 public class Transaction {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer transactionId;
 	private String transactionType;
 	private LocalDate transactionDate;
